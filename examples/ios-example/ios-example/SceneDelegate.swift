4429aca6216532b9499717471c93fe44bca3d93b
Sergey Muravev <sergey.muravev@gmail.com>
2020-11-07 15:34:24 +0600

    Pods dependencies have been updated (because new SDK release v1.3.3) + Example app is fixed to follow v1.3.3 SDK lifecycle (not required to execute `ConfigWiseSDK.reconnect()` function for now).

--- examples/ios-example/ios-example/SceneDelegate.swift
+++ examples/ios-example/ios-example/SceneDelegate.swift
@@ -60,7 +60,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        ConfigWiseSDK.reconnect()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
